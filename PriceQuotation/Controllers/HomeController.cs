b16cf72f8e3ecb572af329a88762119873b5ad28
Suresh <suresh.thagunna77@gmail.com>
2024-05-31 14:28:17 -0400

    remove privacy view

--- PriceQuotation/Controllers/HomeController.cs
+++ PriceQuotation/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace PriceQuotation.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
